e0b18cda71faa95209e371c5d6a904397e77addf
Rishikumar <jaisriandugala207>
2026-03-22 08:13:25 +0530

    my  second commit

--- withdraw-service/src/main/java/com/withdraw_service/kafka/WithDrawRequestEventProducer.java
+++ withdraw-service/src/main/java/com/withdraw_service/kafka/WithDrawRequestEventProducer.java
@@ -1,21 +1,15 @@
 package com.withdraw_service.kafka;
-import com.withdraw_service.config.MyKafkaTopicConfiguration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
 
 import com.statebank.Withdraw;
+import com.withdraw_service.config.MyKafkaTopicConfiguration;
 
-
-@Service
-public class WithDrawRequestProducer {
-
-   
-	
+public class WithDrawRequestEventProducer {
   @Autowired
   KafkaTemplate<String,Withdraw> kafkaTemplate;
-  
   public void requestWithDraw(Withdraw withdraw) {
-	  kafkaTemplate.send(MyKafkaTopicConfiguration.mytopic,withdraw);
+	  kafkaTemplate.send(MyKafkaTopicConfiguration.mytopic, withdraw);
   }
 }
